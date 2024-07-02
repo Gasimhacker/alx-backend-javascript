a8be10164ee0679856f4fec29b28ce41d50e0e32
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-02 23:34:26 +0000

    Use a regular loop

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,6 @@
 export default function hasValuesFromArray(set, arr) {
-  const set2 = new Set(arr);
-  return set2.isSubsetOf(set);
+  for (const item of arr) {
+    if (!set.has(item)) { return false; }
+  }
+  return true;
 }
