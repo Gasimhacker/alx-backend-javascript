cd2f775d0572c49f91a274b721b6c835bcbcb995
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-14 16:32:08 +0000

    Use a try catch block

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(`Error: ${error.message}`);
-  } finally {
-    queue.push('Guardrail was processed');
+    queue.push(mathFunction());
+  } catch ({ name, message }) {
+    queue.push(`${name}: ${message}`);
   }
+  queue.push('Guardrail was processed');
   return queue;
 }
