586669020fa764a6d8458702069136457c373f51
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-23 00:29:23 +0000

    Create a small HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,10 @@
-const http = require('http');
+const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello Holberton School!');
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
-
-app.listen(port, hostname, () => {
-});
+app.listen(1245, '127.0.0.1');
 
 module.exports = app;
