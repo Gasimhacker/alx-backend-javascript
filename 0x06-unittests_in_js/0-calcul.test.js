236e226d5a3664e154fb0fcd64c0be86255c57b7
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-01 09:46:19 +0000

    Try rounding only the second number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,19 +1,26 @@
-const calculateNumber = require("./0-calcul.js");
+const { describe, it } = require('mocha');
+
 const assert = require('assert');
-describe('calculateNumber', function () {
-    it('Should add 2 normal integers', function () {
-      assert.equal(calculateNumber(1, 2), 3);
-    });
-    it('Should round 0.5 to 1', function () {
-      assert.equal(calculateNumber(1.5, 2), 4);
-    });
-    it('Should round 1.6 to 2', function () {
-      assert.equal(calculateNumber(1.6, 2), 4);
-    });
-    it('Should round 1.4 to 1', function () {
-      assert.equal(calculateNumber(1.4, 2), 3);
-    });
-    it('Should add to rounded numbers', function () {
-      assert.equal(calculateNumber(1.5, 1.5), 4);
-    });
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should add 2 normal integers', () => {
+    assert.strictEqual(calculateNumber(1, 2), 3);
+  });
+
+  it('should round 0.5 to 1', () => {
+    assert.strictEqual(calculateNumber(1.5, 2), 4);
+  });
+
+  it('should round 1.6 to 2', () => {
+    assert.strictEqual(calculateNumber(1.6, 2), 4);
+  });
+
+  it('should round 1.4 to 1', () => {
+    assert.strictEqual(calculateNumber(2, 1.4), 3);
+  });
+
+  it('should add to rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1.5, 1.5), 4);
+  });
 });
