c1e8f1b497cf324ce59c5bfcc8157480a4bf9c86
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-02 02:41:43 +0000

    Fix eslint errors

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,10 +6,10 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    it('should make sure that the math used for sendPaymentRequestToApi is the same as Utils.calculateNumber', () => {
-      const calculateNumber = sinon.spy(Utils, 'calculateNumber');
-      sendPaymentRequestToApi(1.5, 2);
-      assert(calculateNumber.calledOnce);
-      calculateNumber.restore();
-    });
+  it('should make sure that the math used for sendPaymentRequestToApi is the same as Utils.calculateNumber', () => {
+    const calculateNumber = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(1.5, 2);
+    assert(calculateNumber.calledOnce);
+    calculateNumber.restore();
+  });
 });
