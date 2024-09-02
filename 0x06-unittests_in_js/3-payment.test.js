c3fe8fa1d8e3063a6aebbc7f1897b0ae6f5d9202
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-02 02:51:47 +0000

    Test that calculateNumber is called with the right arguments

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require('mocha');
 
-const assert = require('assert');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
@@ -8,8 +8,13 @@ const sendPaymentRequestToApi = require('./3-payment');
 describe('sendPaymentRequestToApi', () => {
   it('should make sure that the math used for sendPaymentRequestToApi is the same as Utils.calculateNumber', () => {
     const calculateNumber = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(1.5, 2);
-    assert(calculateNumber.calledOnce);
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumber.calledOnce).to.be.true;
+    expect(calculateNumber.firstCall.args[0]).to.equal('SUM');
+    expect(calculateNumber.firstCall.args[1]).to.equal(100);
+    expect(calculateNumber.firstCall.args[2]).to.equal(20);
+
     calculateNumber.restore();
   });
 });
