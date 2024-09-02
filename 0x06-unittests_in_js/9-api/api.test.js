b93d5ae5f19a05f430ebfdfd5b1126d97d987497
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-02 09:30:21 +0000

    Add unit tests for the main route

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -36,3 +36,29 @@ describe('GET /cart/:id', () => {
     });
   });
 });
+
+describe('Index page', () => {
+  it('should return the Correct status code', (done) => {
+    request('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+
+      done();
+    });
+  });
+
+  it('should return the Correct result', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.contain('Welcome to the payment system');
+
+      done();
+    });
+  });
+
+  it('should return the Correct body length', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.headers['content-length']).to.equal('29');
+
+      done();
+    });
+  });
+});
