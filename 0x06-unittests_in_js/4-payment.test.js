1c90b6ee4dfe9beeda19d50d51b20d9da5a8c411
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-02 03:17:17 +0000

    Stub the function Utils.calculateNumber

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,25 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const { describe, it } = require('mocha');
 
-describe("sendPaymentRequestToApi", function() {
-    it("Check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
-    });
+describe('sendPaymentRequestToApi', () => {
+  it('should make sure that the math used for sendPaymentRequestToApi is the same as Utils.calculateNumber', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const log = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.firstCall.args[0]).to.equal('SUM');
+    expect(calculateNumberStub.firstCall.args[1]).to.equal(100);
+    expect(calculateNumberStub.firstCall.args[2]).to.equal(20);
+
+    expect(log.calledOnce).to.be.true;
+    expect(log.firstCall.args[0]).to.equal('The total is: 10');
+
+    log.restore();
+    calculateNumberStub.restore();
+  });
 });
