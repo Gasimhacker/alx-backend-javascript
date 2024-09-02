09879ffd7857a86a6d960679e3535100a44cec26
Mohamed Ahmed <ma2286572@gmail.com>
2024-09-02 05:18:16 +0000

    Use Hooks inside the unit test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,23 +1,29 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./5-payment");
-const expect = require("chai").expect;
+const {
+  describe, it, afterEach, beforeEach,
+} = require('mocha');
 
-describe("sendPaymentRequestToApi", function() {
-    beforeEach("SetUp Spy to use for each test", function() {
-	sinon.spy(console, "log");
-    });
-    afterEach("Restore Spy after each test", function() {
-	console.log.restore();
-    });
-    it("Check that console.log is called with the right argument", function() {
-	sendPaymentRequestToApi(100, 20);
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
 
-	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
-    });
-    it("Check that console.log is called with the right argument", function() {
-	sendPaymentRequestToApi(10, 10);
+describe('sendPaymentRequestToApi', () => {
+  beforeEach(() => {
+    sinon.spy(console, 'log');
+  });
 
-	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
-    });
+  afterEach(() => {
+    console.log.restore();
+  });
+
+  it('should Verify that the console is logging the string "The total is: 120"', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(console.log.withArgs('The total is: 120').calledOnce).to.be.true;
+  });
+
+  it('should Verify that the console is logging the string "The total is: 20"', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(console.log.withArgs('The total is: 20').calledOnce).to.be.true;
+  });
 });
